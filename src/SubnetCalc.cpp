677b1ef0085a5a461d00680d42f65cd415d557bc
Hoverpi <enrique.rios@iteso.mx>
2024-09-24 08:28:59 -0600

    finish-genSub

--- src/SubnetCalc.cpp
+++ src/SubnetCalc.cpp
@@ -31,5 +31,33 @@ void calcSubnets(unsigned int dSubnets = 0, unsigned int dHosts) {
                 subnets.push_back(make_network_v4(subnetAddress, newPrefix));
             }
         }
+
+        // Calcular subredes basadas en la cantidad de hosts
+        if (dHosts > 0) {
+            unsigned int bitsNeed = 0;
+            // Determinar cuántos bits se necesitan para los hosts
+            while ((1 << bitsNeed) < (dHosts + 2)) {  // Se suman 2 para la IP de red y la de broadcast
+                bitsNeed++;
+            }
+
+            // Nuevo prefijo para las subredes
+            unsigned short newPrefix = 32 - bitsNeed;
+
+            // Generar subredes
+            unsigned int numSubnets = 1 << (newPrefix - prefix);
+            for (unsigned int i = 0; i < numSubnets; ++i) {
+                address_v4 subnetAddress = network.network().to_v4();
+                subnetAddress = address_v4(subnetAddress.to_ulong() + (i << bitsNeed));
+                subnets.push_back(make_network_v4(subnetAddress, newPrefix));
+            }
+        }
+
+        // Imprimir las subredes calculadas
+        for (const auto& subnet : subnets) {
+            cout << "Subnet: " << subnet << endl;
+        }
+
+    } catch(system_error &e) {
+        cerr << "Error: IP no válida o prefijo incorrecto: " << e.what() << endl;
     }
 }
